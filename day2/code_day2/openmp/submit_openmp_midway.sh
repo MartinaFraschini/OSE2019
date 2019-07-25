450b70e4bda42611787ee1b022b13468017727a0
Martina Fraschini <martina.fraschini@unil.ch>
2019-07-25 17:17:43 -0500

    Add solutions to homeworks

--- day2/code_day2/openmp/submit_openmp_midway.sh
+++ day2/code_day2/openmp/submit_openmp_midway.sh
@@ -14,7 +14,7 @@
 
 
 # and request 8 cpus per task for OpenMP threads
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=20
 
 # set OMP_NUM_THREADS to the number of --cpus-per-task we asked for
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
---
+++ day3/Exercise_day3/OpenMP/Exercise1/submit_openmp_midway.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# set the job name
+#SBATCH --job-name=normalize_vec
+
+# send output to normalize_vec.out
+#SBATCH --output=normalize_vec.out
+
+# this job requests node
+#SBATCH --ntasks=1
+
+# and request cpus per task for OpenMP threads
+#SBATCH --cpus-per-task=1
+
+# set OMP_NUM_THREADS to the number of --cpus-per-task we asked for
+export OMP_NUM_THREADS=1
+
+# Run the process with mpirun. Notice -n is not required. mpirun will
+# automatically figure out how many processes to run from the slurm options
+### openmp executable
+./normalize_vec.exec
