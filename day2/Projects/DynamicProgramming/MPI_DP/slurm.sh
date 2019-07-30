336cb250d7e6f1f8da34d5ae88d9192932b0cbe4
Martina Fraschini <martina.fraschini@unil.ch>
2019-07-29 20:10:09 -0500

    Add solutions to homeworks

---
+++ day2/Projects/DynamicProgramming/MPI_DP/slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=BS	## job name
+
+#SBATCH --output=BS.out		## output file
+
+#SBATCH --ntasks=10		# this job requests 1 core
+
+#SBATCH --partition=broadwl		# partition to use
+
+# Run the process 
+mpiexec -np $SLURM_NTASKS ./VFI
