3fb1585a31cc14ac1ca61aebbc57dc09e4ec0d13
rccguest 1420 <rccguest1420@midway2-login2.rcc.local>
2019-07-29 21:05:53 -0500

    Add solutions to homeworks

--- day2/Projects/DynamicProgramming/MPI_DP/slurm.sh
+++ day2/Projects/DynamicProgramming/MPI_DP/slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=BS	## job name
+#SBATCH --job-name=VFI	## job name
 
-#SBATCH --output=BS.out		## output file
+#SBATCH --output=VFI.out		## output file
 
 #SBATCH --ntasks=10		# this job requests 1 core
 
